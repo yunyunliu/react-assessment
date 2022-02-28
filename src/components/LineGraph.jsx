7d66f2e06efbbacf56e1735a46cd3340ae30399d
Yunyun Liu <celeph4is@gmail.com>
2022-02-28 09:23:10 -0800

    save progress

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -4,6 +4,7 @@ import 'chartjs-adapter-date-fns';
 
 const LineGraph = ({ parameter }) => {
     const chartContainer = useRef(null);
+
     const [chartInstance, setChartInstance] = useState(null);
     const [rawData, setRawData] = useState(null);
     const [xAxisLabels, setXAxisLabels] = useState(null);
@@ -18,10 +19,12 @@ const LineGraph = ({ parameter }) => {
             .then(result => {
                 const data = result.results;
                 setRawData(data);
+                // format data in {x,y} format for scatter plot
                 const formatted = data.map(current => {
                     return { x: current.date.local, y: current.value };
                 });
                 setFormattedData(formatted);
+                // get all unique dates from dataset to use as labels in chart
                 const labels = [];
                 data.forEach(current => {
                     const timestamp = current.date.utc;
@@ -33,54 +36,54 @@ const LineGraph = ({ parameter }) => {
                 setXAxisLabels(labels);
             });
     }, []);
-
-    // get all unique dates from dataset to use as labels in chart
-
      // data and options for chart
-        // const chartConfigs = {
-        //     type: 'scatter',
-        //     data: {
-        //         datasets: [
-        //             {
-        //                 // label: "Parameter: " + parameter.toUpperCase(),
-        //                 data: formattedData,
-        //                 backgroundColor: 'rgb(255, 99, 132)'
-        //             }
-        //         ]
-        //     },
-        //     options: {
-        //         scales: {
-        //             x: {
-        //                 type: 'time',
-        //                 labels: getLabels(rawData),
-        //                 time: { unit: 'day' },
-        //                 title: {
-        //                     display: true,
-        //                     text: 'Date'
-        //                 }
-        //             },
-        //             y: {
-        //                 title: {
-        //                     display: true,
-        //                     // text: parameter.toUpperCase()
-        //                 },
-        //                 ticks: { beginAtZero: true }
-        //             }
-        //         }
-        //     }
-        // }
-     // create chart
-    // if (chartContainer && chartContainer.current) {
-    //     const newChart = new Chart(chartContainer.current, chartConfigs);
-    //     setChartInstance(newChart);
-    //     newChart.update();
-    // }
+    const chartConfigs = {
+        type: 'scatter',
+        data: {
+            datasets: [
+                {
+                    // label: "Parameter: " + parameter.toUpperCase(),
+                    data: formattedData,
+                    backgroundColor: 'rgb(255, 99, 132)'
+                }
+            ]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    labels: xAxisLabels,
+                    time: { unit: 'day' },
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        // text: parameter.toUpperCase()
+                    },
+                    ticks: { beginAtZero: true }
+                }
+            }
+        }
+    }
+    // create chart
+    if (chartContainer && chartContainer.current) {
+        if (chartInstance) {
+            chartInstance.destroy();
+        }
+        // chartInstance.destroy();
+        const newChart = new Chart(chartContainer.current, chartConfigs);
+        setChartInstance(newChart);
+        // newChart.update();
+    }
     if (rawData) {
         return (
             <div>
-                {console.log(xAxisLabels)}
+                {/* {console.log(xAxisLabels)} */}
                 <canvas
-                    id="myChart"
                     ref={chartContainer}
                 />
             </div>
